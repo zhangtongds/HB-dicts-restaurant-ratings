203ae9af8452107328827a5da2ef0908d1c94f2c
Hackbright Student <no-reply@hackbrightacademy.com>
2018-04-09 17:17:05 -0700

    having trouble sorting additional items

--- ratings.py
+++ ratings.py
@@ -1,8 +1,10 @@
 import sys
 
+restaurant_ratings = {}
+
 def restaurant_rating(filename):
-    restaurant_ratings = {}
     """Restaurant rating lister."""
+    #restaurant_ratings = {}
 
     with open(filename) as f:
         for line in f:
@@ -11,29 +13,22 @@ def restaurant_rating(filename):
             value = line[-2]
             restaurant_ratings[key] = value
             # print "{} is rated at {}.".format(key, value)
-
         # print restaurant_ratings
         return restaurant_ratings
 
-# def sorted_restaurant_rating(restaurant_ratings):
-#     """ Take the dictionary and return it sorted """
-
-#     # restaurant_ratings = restaurant_rating(filename)
-#     restaurant_names = sorted(restaurant_ratings)
-#     for restaurant in restaurant_names:
-#         print "{} is rated at {}.".format(restaurant, restaurant_ratings[restaurant])
-
-# restaurant_ratings = restaurant_rating(sys.argv[1])
-# sorted_restaurant_rating(restaurant_ratings)
-
-
-
-def sorted_restaurant_rating(filename):
+def sorted_restaurant_rating(restaurant_ratings):
     """ Take the dictionary and return it sorted """
     rest_lst = restaurant_ratings.items()
     sorted_rest_lst = sorted(rest_lst)
     for restaurant, rating in sorted_rest_lst:
         print "{} is rated at {}.".format(restaurant, rating)
 
+def add_ratings():
+    restaurant_name = raw_input("Please input a restaurant name: ")
+    rating = raw_input("Please input rating for {}: ".format(restaurant_name))
+    restaurant_ratings[restaurant_name] = rating
+    return restaurant_ratings
+
+add_ratings()
 restaurant_ratings = restaurant_rating(sys.argv[1])
 sorted_restaurant_rating(restaurant_ratings)
