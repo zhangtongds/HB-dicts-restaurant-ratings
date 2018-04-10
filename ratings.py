67250db6d4074eee30903e75ecf7a030d5ad935d
Tong Zhang <zhangtong.purdue@gmail.com>
2018-04-09 20:55:14 -0700

    Done with random and user choice of restaurant to update rating.

--- ratings.py
+++ ratings.py
@@ -1,5 +1,5 @@
 import sys
-
+from random import choice
 # restaurant_ratings = {}
 
 def restaurant_rating(filename):
@@ -36,23 +36,60 @@ def add_ratings(restaurant_ratings):
             else:
                 rating = raw_input("Please enter a rating between 1-5: ")
         except ValueError:
-            rating = raw_input("Please enter a number.")
+            rating = raw_input("Please enter a number: ")
             
-            
+def random_update_rating(restaurant_ratings):
+    restaurant = choice(restaurant_ratings.items())
+    print "{} is chosen, the rating is {}.".format(restaurant[0], restaurant[1])
+    updated_rating = raw_input("Please enter a number 1 - 5 to update the rating: ")
+    nums = [1, 2, 3, 4, 5]
+    while True:
+        try:
+            if int(updated_rating) in nums:
+                restaurant_ratings[restaurant[0]] = updated_rating
+                break
+            else:
+                updated_rating = raw_input("Please enter a updated_rating between 1-5: ")
+        except ValueError:
+            updated_rating = raw_input("Please enter a number to update the rating: ")
+    return restaurant_ratings
 
-
-            # rating = raw_input("Please input rating for {}: ".format(restaurant_name))
-            #     if rating in nums:
-            #         restaurant_ratings[restaurant_name] = rating
-            #         break
-
-# add_ratings()
-# restaurant_rating(sys.argv[1])
-# sorted_restaurant_rating()
+def user_update_rating(restaurant_ratings):
+    restaurants = restaurant_ratings.items()
+    print "Please choose the restaurants below: "
+    for key, value in restaurants:
+        print key
+    user_choice = raw_input("Please choose a restaurant: ")
+    
+    while True:
+        if user_choice in restaurant_ratings.keys():
+            print "You chose {}, the rating is {}.".format(user_choice, restaurant_ratings[user_choice])
+            updated_rating = raw_input("Please update the rating for {}: ".format(user_choice))
+            nums = [1, 2, 3, 4, 5]
+            while True:
+                try:
+                    if int(updated_rating) in nums:
+                        restaurant_ratings[user_choice] = updated_rating
+                        print "The updated rating for {} is {}. ".format(user_choice, updated_rating)
+                        print "======================="
+                        print "The sorted list of restaurants: "
+                        break
+                    else:
+                        updated_rating = raw_input("Please enter a updated_rating between 1-5: ")
+                except ValueError:
+                    updated_rating = raw_input("Please enter a number to update the rating: ")
+            break
+        else:
+            user_choice = raw_input("Please choose a restaurant WITHIN the list: ")
 
 
 ratings = restaurant_rating(sys.argv[1])
 
-add_ratings(ratings)
+# add_ratings(ratings)
+
+# random_update_rating(ratings)
+
+
+user_update_rating(ratings)
 
 sorted_restaurant_rating(ratings)
